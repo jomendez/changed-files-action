617657915e12f5beda1b7c65dd05870f695a7089
pepe <pepe@DESKTOP-JT5G5K0>
2021-04-26 12:48:29 -0400

    fixed test failling

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -4,7 +4,13 @@ test.each([
   [['config.json', 'package.json', 'package-lock.json'], ['package.json', 'package-lock.json'], true],
   [['config.json', 'package.json', 'package-lock.json'], ['package.json'], true],
   [['config.json', 'package.json', 'package-lock.json'], ['package-lock.json'], true],
+  [['config.json', 'script.js', 'index.html'], ['package-lock.json'], true],
+  [['config.json', 'script.js', 'index.html'], ['package.json'], true],
   [['config.json', 'package.json', 'package-lock.json'], ['config.json'], false],
+  [['package.json'], ['package.json'], false],
+  [['package-lock.json'], ['package-lock.json'], false],
+  [['package.json'], ['package-lock.json'], false],
+  [['package-lock.json'], ['package.json'], false],
   [['config.json', 'package.json', 'package-lock.json'], [], false]
 ])('.packageFileModifiedWithOtherFiles(%s, %s)', (all, modified, expected) => {
   expect(packageFileModifiedWithOtherFiles(all, modified)).toBe(expected)
